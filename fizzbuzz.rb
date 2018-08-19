c5a41c14caef67444078949fa0f2161cbe595628
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-19 20:54:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,11 @@
 def fizzbuzz(int)
   if int%3==0 
     return "Fizz"
-  elsif
+  else if int%5==0 
+    return "Buzz"
+  else if (int%3==0 && int%5==0)
+    return "Fizzbuzz"
+  else
+    return nil
 end
 end
