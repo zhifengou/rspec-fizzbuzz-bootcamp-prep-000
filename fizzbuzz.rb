dcc5f2e0523d35ab3f8eb67e05273479c30c2765
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-19 21:17:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3==0 && int % 5==0 
-  "FizzBuz"
+  "FizzBuzz"
   elsif int%5==0 
     "Buzz"
   elsif int % 3==0
-  "Fizzz"
+  "Fizz"
   else
     nil
   end
