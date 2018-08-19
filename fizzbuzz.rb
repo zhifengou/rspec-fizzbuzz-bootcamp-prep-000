b675cb819e3425cf0ed1effc91cea44c5aafbbfd
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-19 21:08:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,3 +11,14 @@
 #     nil
 #   end
 # end
+def fizzbuzz(int)
+  if int % 3==0 && int % 5==0
+    "FizzBuzz"
+  elsif int % 3==0
+    "Fizz"
+  elsif int % 5==0
+    "Buzz"
+  else
+    nil
+  end
+end
